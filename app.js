9a797dd5b43c89e48dce1722e545e6c152b896bb
howardmann <howardmann27@gmail.com>
2017-01-30 23:30:19 +1100

    Edit body parser to accept ember jsonAPI post requests

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ var app = express();
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
   next();
 });
 
@@ -34,7 +35,7 @@ app.set('view engine', 'hbs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
