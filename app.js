62d8e7441bc1cdc34c837933982879534f09378a
howardmann <howardmann27@gmail.com>
2017-01-30 21:32:50 +1100

    Add CORS specification to enable public sharing

--- app.js
+++ app.js
@@ -17,6 +17,13 @@ var flash = require('connect-flash');
 // Load express app
 var app = express();
 
+// ENABLE CORS specificiation standard script from enable-cors https://enable-cors.org/server_expressjs.html
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // ===BP: EXPRESS-HANDLEBARS
 app.engine('hbs', require('express-handlebars')({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}));
 
