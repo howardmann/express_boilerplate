4d987d0de46d631404e5e9f2862288cb2a9fbf23
howardmann <howardmann27@gmail.com>
2017-03-19 19:50:40 +1100

    Change bodyParser to pass mocha tests

--- app.js
+++ app.js
@@ -35,7 +35,8 @@ app.set('view engine', 'hbs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+app.use(bodyParser.json());
+// app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
